43a89b72cb9a4ae4996f491d8f75dfa7ad82b3cc
reionwong <reionwong@gmail.com>
2026-09-04 00:24:04 -0400

    fix(screen): register Output as a QML namespace

--- screen/output.h
+++ screen/output.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <QtQml/qqmlregistration.h>
 
 class Output
 {
@@ -11,8 +12,16 @@ public:
         None = 1,
         Left = 2,
         Inverted = 4,
-        Right = 8,
+        Right = 8
     };
     Q_ENUM(Rotation)
 };
 
+// A Q_GADGET registered directly would become a QML value type, and those have
+// to be named in lowercase. Exposing it as a namespace keeps `Output.Left`.
+namespace OutputEnums
+{
+Q_NAMESPACE
+QML_FOREIGN_NAMESPACE(Output)
+QML_NAMED_ELEMENT(Output)
+}
--- screen/screen.h
+++ screen/screen.h
@@ -3,12 +3,14 @@
 #include <KScreen/Config>
 
 #include <QObject>
+#include <QtQml/qqmlregistration.h>
 
 class KScreenOutputModel;
 
 class KScreenScreen : public QObject
 {
     Q_OBJECT
+    QML_NAMED_ELEMENT(Screen)
     Q_PROPERTY(KScreenOutputModel *outputModel READ outputModel NOTIFY outputModelChanged)
 
 public:
